f80e4140c1d5711d7809939e4dc4c887d3c97fbb
Lucas Gothelipe <lucasqg@outlook.com>
2023-08-23 18:23:38 +1000

    fix count

--- vpc-peering.tf
+++ vpc-peering.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc_peering_connection" "requester" {
-  count         = local.create_requester ? 0 : 1
+  count         = local.create_requester ? 1 : 0
   peer_vpc_id   = var.accepter_vpc_id
   peer_owner_id = var.accepter_owner_id
   vpc_id        = var.requester_vpc_id
@@ -7,7 +7,7 @@ resource "aws_vpc_peering_connection" "requester" {
 }
 
 resource "aws_vpc_peering_connection_accepter" "accepter" {
-  count                     = local.create_accepter ? 0 : 1
+  count                     = local.create_accepter ? 1 : 0
   provider                  = aws.accepter
   vpc_peering_connection_id = var.mode == "complete" ? aws_vpc_peering_connection.requester[0].id : var.requester_connection_id
   auto_accept               = true
